7b2f643fb6412950fffe2dd31378808c40be18b3
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-02-21 15:44:48 -0300

    type-conversion

---
+++ TypeConversion/typeConversion.js
@@ -0,0 +1,7 @@
+let nome = 'Tanjirou'
+console.log(`${nome} tem ${nome.length} letras`)
+
+let number = 12345678
+console.log(`${number} tem ${number.length} digitos`)
+
+console.log(`${number} tem ${String(number).length} digitos`)
