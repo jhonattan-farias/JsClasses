5d0cfac69be8d465805185be061b9f385a776179
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-02-24 23:01:00 -0300

    Create ternary.js

---
+++ condicionalTernario/ternary.js
@@ -0,0 +1,7 @@
+/*### O operador condicional ternário é uma forma de simples de criar uma condição. 
+O sistema funciona assim.
+Condição ? se sim(executa isso) : senão(executa isso) */
+
+const valor = 43
+const resultado = valor == 1 ? 'valor é igual a 1' : `Valor é igual a ${valor}`
+console.log(resultado) 
