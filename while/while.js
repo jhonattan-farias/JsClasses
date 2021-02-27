181fcdd7d2685a3358377ce49f0eb9df8575b0a6
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-02-27 20:40:05 -0300

    Create while.js

---
+++ while/while.js
@@ -0,0 +1,15 @@
+//O while significa (enquanto). O while é um uma estrutura de repetição
+//parecida com o for. O while recebe um valor em seus parâmetro. 'Enquanto' aquele valor
+//for true, ele executa a função dentro do while. Exemplo
+let i = 20
+
+while(i > 0){
+console.log(i)
+i--
+}
+//a variavel i recebe o valor 20. Enquanto i for maior que 0, ou seja,true.
+//A função while será executada até que o i não seja maior que 0.
+
+
+
+
