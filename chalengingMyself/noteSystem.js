c58543380a3596b499954b5d4d84230c974e82e5
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-02-28 21:13:20 -0300

    Update noteSystem.js

--- chalengingMyself/noteSystem.js
+++ chalengingMyself/noteSystem.js
@@ -1,31 +1,14 @@
 const darNota = (studentNote) => {
-    var note = ''
 
     switch(studentNote){
 
         case studentNote >= 90:
-        note = 'A'
+        console.log('A')
         break;
 
-        case studentNote >= 80:
-         note = 'B'
-        break;
-
-        case studentNote >= 70:
-        note = 'C'
-        break;
-
-        case studentNote >= 60:
-        note = 'D'
-        break ;
-        
-        case studentNote < 60:
-        note = 'F'
-        break;
 
         default:
             break;
     }
-    return note
 }
-console.log(darNota(50))
+darNota(91)
