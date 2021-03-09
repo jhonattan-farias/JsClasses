7a2d6ef782601cb90b4af2c7c48ece7ccf2aa5a0
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-08 22:47:01 -0300

    Update fah-to-celsius.js

--- chalengingMyself/fah-to-celsius.js
+++ chalengingMyself/fah-to-celsius.js
@@ -1,18 +1,20 @@
 const toCelsius = (F) =>
 { 
     const C = ( F - 32) * 5/9
-    return console.log(C)
+    console.log(C)
 }
 
 const toFahrenheint = (C) =>
 {
     const F = (C * 9/5) + 32
-    return console.log(F)
+    console.log(F)
 }
 
 const convert = (type,value) => 
 {
-    type === 'F' ? toCelsius(value) : toFahrenheint(value)     
+    type === 'F' ? 
+    toCelsius(value) : 
+    toFahrenheint(value)     
 }
 
-convert('F',30)
+convert('C',30)
